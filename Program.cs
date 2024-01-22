f8d3bf3a670409a030bd2a448079ddc42748a627
Academy-User <Academy-User@DESKTOP-1L3B0TP>
2024-01-22 11:09:47 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Draw_a_shape
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("         /|");
+            Console.WriteLine("        / |");
+            Console.WriteLine("       /  |");
+            Console.WriteLine("      /   |");
+            Console.WriteLine("     -----");
+            
+            // to pause the console else it closes on its own!
+            Console.ReadLine();
+
+        }
+    }
+}
